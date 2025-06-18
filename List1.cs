ca4c2cb1a0613e2367f8a77b27ae1d7e01de7930
Erkan <erkankrgz0707@gmail.com>
2025-06-18 04:38:50 +0300

    Add files via upload

---
+++ List1.cs
@@ -0,0 +1,13 @@
+﻿List<string> davetliler = new List<string>();
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Eklemek istediğiniz davetli:");
+    string davetli = Console.ReadLine();
+    davetliler.Add(davetli);
+}
+Console.WriteLine("-------- BALONUN DAVETLİLERİ --------");
+foreach (var davetli in davetliler)
+{
+    string yazdir = char.ToUpper(davetli[0]) + davetli[1..].ToLower();  
+    Console.WriteLine(yazdir);
+}
